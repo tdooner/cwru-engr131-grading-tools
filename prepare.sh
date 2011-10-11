7a00208da8ea994a1b25e2e90b1dd552f39d7874
Tom Dooner <tom@Toms-MacBook-Pro.local>
2011-10-11 17:52:15 -0400

    Initial Commit

---
+++ prepare.sh
@@ -0,0 +1,24 @@
+# Usage:  ./prepare.sh [folder_with_gradebook*.zip] [/path/to/students.txt]
+
+if [ $# -ne 2 ]; then
+	echo "Usage:  ./prepare.sh [folder_with_gradebook*.zip] [/path/to/students.txt]"
+	exit
+fi
+
+# First unzip the gradebook file...
+unzip $1/gradebook*.zip -d $1
+
+# Then remove all files which aren't in students.txt
+find $1 -type f | grep -v -F -f $2 | grep -v gradebook | xargs rm
+
+# Now move all students' files into their own folder.
+for CASEID in `find $1 -type f | grep -v gradebook | cut -d "_" -f 2 | uniq`; do
+	mkdir $1/$CASEID/
+	for J in `find $1 -type f -name \*$CASEID\*`; do
+		BASENAME=`basename $J`
+		ACTUAL_FILENAME=`echo $BASENAME | cut -d "_" -f 5-`
+		echo "%"$BASENAME > $1/$CASEID/$ACTUAL_FILENAME 
+		cat $J >> $1/$CASEID/$ACTUAL_FILENAME
+		rm $J
+	done
+done
