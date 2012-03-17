4357d23869ef2164851228587ffffedd4c68fdd0
Tom Dooner <tomdooner@gmail.com>
2012-03-16 21:35:38 -0400

    Two changes to prepare.sh
    1. Don't add the header to the files, because that didn't work out.
    2. Add capability of a folder called "everyone" which contains files
    that will be symlink'd into every student's folder.

--- prepare.sh
+++ prepare.sh
@@ -8,20 +8,19 @@ move_to_caseid() {
         #  newer attempts will override older attempts.
         BASENAME=`basename $J`
         ACTUAL_FILENAME=`echo $BASENAME | cut -d "_" -f 5-`
-        TYPE=`file -bI $J | grep -o 'application/octet-stream'`
-        if [[ -z $TYPE ]]; then
-            # If the file isn't a binary blob, append the filename as a comment
-            #   at the top of the file.
-            if [[ -n $ACTUAL_FILENAME ]]; then
-                echo "%"$BASENAME > $1/$2/$ACTUAL_FILENAME 
-                cat $J >> $1/$2/$ACTUAL_FILENAME
-            fi
-            rm $J
-        else
-            # If the file IS a binary blob, just copy it to its proper place.
+        if [[ -n $ACTUAL_FILENAME ]]; then 
             mv $J $1/$2/$ACTUAL_FILENAME
+        else
+            rm $J
         fi
 	done
+    # If there are files in $1/everyone, symlink them in everyone's directory.
+    if [[ -d $1/everyone ]]; then
+        for J in `ls $1/everyone/`; do
+            # If the file was included by the student, use the vanilla version anyway.
+            ln -fs ../everyone/$J $1/$2/$J
+        done
+    fi
 }
 
 if [ $# -ne 2 ]; then
@@ -30,14 +29,14 @@ if [ $# -ne 2 ]; then
 fi
 
 # First unzip the gradebook file...
-unzip $1/gradebook*.zip -d $1 -q
+unzip -q -d $1 $1/gradebook*.zip
 
 # Then remove all files which aren't in students.txt
 if [[ -f $2 ]]; then
-    find $1 -type f | grep -v -F -f $2 | grep -v gradebook | xargs rm
+    find $1 -type f | grep -v -F -f $2 | grep -v gradebook | grep -v /everyone/ | xargs rm
 fi
 
 # Now move all students' files into their own folder.
-for CASEID in `find $1 -type f | grep -v gradebook | cut -d "_" -f 2 | uniq`; do
+for CASEID in `find $1 -type f | grep -v gradebook | grep -v /everyone/ | cut -d "_" -f 2 | uniq`; do
     move_to_caseid $1 $CASEID &
 done
