7574ae7fead44e79fae832cf66b25a3c059a67c8
Tom Dooner <tomdooner@gmail.com>
2012-07-21 17:12:52 -0700

    Improve MATLABRunner return values and documentation.

--- PythonSubmit/app.py
+++ PythonSubmit/app.py
@@ -3,6 +3,7 @@ from time import sleep
 import os.path
 from os import listdir, remove
 import subprocess
+import glob
 
 NUM_SKIP_LINES = 11
 MATLAB_PATH = "/Applications/MATLAB_R2011a.app/bin/matlab"
@@ -13,33 +14,54 @@ class MATLABRunner:
         pass
 
     def run_matlab(self, matlab_str, path=os.path.abspath(__file__)):
+        '''
+        Runs an arbitrary MATLAB string of commands, e.g.
+            run_matlab('disp("hi"); disp("hi again");')
+
+        Optionally accepts a path to append to the MATLABPATH, so arbitrary
+        user functions can be used.
+        '''
         matlab_str = matlab_str + "; exit;"
         cmd = [MATLAB_PATH] + MATLAB_ARGS + ['-r', matlab_str]
-        status, out, err = self.execute_command(cmd, timeout=60, options={'MATLABPATH': path})
-        print "Got " + str(len(out.split("\n"))) + " lines on stdout"
-        print "Got " + str(len(err.split("\n"))) + " lines on stderr"
+        status, out, err = self.execute_command(cmd,
+                timeout=90,
+                options={'MATLABPATH': path})
         if status == 0:
             actual_output = out.split("\n")[(NUM_SKIP_LINES-1):]
-            return "\n".join(actual_output)
+            return ("\n".join(actual_output), err)
         else:
             raise MATLABException(err)
-            return out
+            return None
 
     def publish_matlab(self, matlab_str):
+        '''
+        Runs a given MATLAB string through MATLAB's publish() function. Use
+        this to capture graphical output.
+            publish_matlab("x=-1:0.1:1; y=sin(x); plot(x,y);")
+        '''
         tmp_dir = mkdtemp()
         m_file_name = mkstemp(suffix=".m", dir=tmp_dir)[1]
         m_file = open(m_file_name, "w")
         m_file.write(matlab_str)
         m_file.close()
         matlab_str = "publish('" + m_file_name + "')"
-        print m_file_name
-        print self.run_matlab(matlab_str, path=tmp_dir)
-        print listdir(tmp_dir)
-        os.remove(m_file_name)
+        out, err = self.run_matlab(matlab_str, path=tmp_dir)
+
+        # Now, deal with the output:
+        images = glob.glob(os.path.join(
+            tmp_dir,
+            'html',
+            os.path.basename(m_file_name).split(".")[0] + '_*.png'))
+        return {
+            'images': images,
+            'm_file': m_file_name,
+            'stdout': out,
+            'stderr': err
+        }
 
     def execute_command(self, cmd, timeout=0, options={}):
         """
-        Run an arbitrary command with an optional timeout (in seconds)
+        Run an arbitrary command with an optional timeout (in seconds).
         """
         print "Running " + " ".join(cmd)
         for k,v in options.items():
@@ -70,4 +92,5 @@ class TimeoutException(Exception):
 if __name__ == '__main__':
     mlr = MATLABRunner()
     #print mlr.run_matlab("disp('hi')")
-    print mlr.publish_matlab("x=-1:0.1:1;y=sin(x);plot(x,y);");
+    res = mlr.publish_matlab("x=-1:0.1:1;y=sin(x);plot(x,y);");
+    print res
