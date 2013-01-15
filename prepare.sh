3231ba3b897eabedac489c631b6f405e8d8878a0
Tom Dooner <tom@causes.com>
2013-01-15 13:35:45 -0500

    Cleanup for S'13

--- prepare.sh
+++ prepare.sh
@@ -1,31 +1,33 @@
 # Usage:  ./prepare.sh [folder_with_gradebook*.zip] [/path/to/students.txt]
+
 move_to_caseid() {
     #$1 is HW10
     #$2 is ted27
-	mkdir $1/$2/
-	for J in `find $1 -type f -name \*$2\*`; do
-        # NOTE: Since the files for multiple attempts are lexicographically ordered, 
-        #  newer attempts will override older attempts.
+    mkdir $1/$2/
+    for J in `find $1 -type f -name \*$2\*`; do
+        # NOTE: Since the files for multiple attempts are lexicographically
+        # ordered, newer attempts will override older attempts.
         BASENAME=`basename $J`
         ACTUAL_FILENAME=`echo $BASENAME | cut -d "_" -f 5-`
-        if [[ -n $ACTUAL_FILENAME ]]; then 
+        if [[ -n $ACTUAL_FILENAME ]]; then
             mv $J $1/$2/$ACTUAL_FILENAME
         else
             rm $J
         fi
-	done
+    done
     # If there are files in $1/everyone, symlink them in everyone's directory.
     if [[ -d $1/everyone ]]; then
         for J in `ls $1/everyone/`; do
-            # If the file was included by the student, use the vanilla version anyway.
+            # If the file was included by the student, use the vanilla version
+            # anyway.
             ln -fs ../everyone/$J $1/$2/$J
         done
     fi
 }
 
 if [ $# -ne 2 ]; then
-	echo "Usage:  ./prepare.sh [folder_with_gradebook*.zip] [/path/to/students.txt]"
-	exit
+    echo "Usage:  ./prepare.sh [folder_with_gradebook*.zip] [/path/to/students.txt]"
+    exit
 fi
 
 # First unzip the gradebook file...
